c5db278d5ba36928acccb7980cd609224cab375e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-21 20:05:49 +0300

    rm nix-dev

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
         let flakes = inputs.flakes.flakes; in
         {
           inherit (flakes.source-flake) nixpkgs flake-utils formatter;
+          inherit (flakes) drv-tools workflows flakes-tools devshell codium;
           haskell-tools = flakes.language-tools.haskell;
-          inherit flakes;
         };
 
-      outputs = outputs_ { } // { inputs = inputs_; outputs = outputs_; };
+      outputs = outputs_ { } // { inherit outputs_; inputs = inputs_; };
 
       outputs_ =
         inputs__:
@@ -19,36 +19,112 @@
           (system:
           let
             pkgs = inputs.nixpkgs.legacyPackages.${system};
+            inherit (inputs.codium.lib.${system}) extensions extensionsCommon settingsNix settingsCommonNix writeSettingsJSON mkCodium;
+            inherit (inputs.devshell.lib.${system}) mkCommands mkRunCommands mkShell;
+            inherit (inputs.flakes-tools.lib.${system}) mkFlakesTools;
             inherit (inputs.haskell-tools.lib.${system}) toolsGHC;
+            inherit (inputs.workflows.lib.${system}) writeWorkflow steps nixCI run stepsIf names os;
+            inherit (inputs.drv-tools.lib.${system}) mkShellApps getExe;
 
             lima = "lima";
 
             override = {
               overrides = self: super: {
                 doctest-parallel = super.doctest-parallel_0_3_0_1;
-                ${lima} = super.callCabal2nix lima ./lima { };
+                ${lima} = super.callCabal2nix lima ./${lima} { };
               };
             };
 
             ghcVersion = "928";
 
+            # Next, set the desired GHC version
             inherit (toolsGHC {
-              version = ghcVersion;
               inherit override;
-            }) haskellPackages;
+              version = ghcVersion;
+              packages = ps: [ ps.${lima} ];
+            }) cabal hls hpack fourmolu ghcid haskellPackages;
 
-            packages = {
-              default = haskellPackages.${lima};
-              sdist = (haskellPackages.buildFromCabalSdist haskellPackages.${lima}).overrideAttrs (_: { pname = "lima-sdist"; });
+            tools = [ hls cabal hpack ghcid fourmolu ];
+
+            packages =
+              let
+                packages1 = mkShellApps { writeDocs.text = "${getExe cabal} test ${lima}:test:readme"; };
+                packages2 = {
+                  default = haskellPackages.${lima};
+                  sdist = (haskellPackages.buildFromCabalSdist haskellPackages.${lima}).overrideAttrs (_: { pname = "lima-sdist"; });
+
+                  codium = mkCodium { extensions = extensionsCommon // { inherit (extensions) haskell; }; };
+                  writeSettings = writeSettingsJSON (settingsCommonNix // { inherit (settingsNix) haskell; });
+                  inherit (mkFlakesTools { dirs = [ "." ]; root = ./.; }) updateLocks pushToCachix format;
+                  writeWorkflows = writeWorkflow "CI"
+                    (
+                      nixCI {
+                        jobArgs = {
+                          doPushToCachix = true;
+                          cacheNixArgs = {
+                            linuxGCEnabled = true;
+                            linuxMaxStoreSize = 5100000000;
+                            macosGCEnabled = true;
+                            macosMaxStoreSize = 5100000000;
+                          };
+                          doUpdateLocks = true;
+                          doCommit = false;
+                          doSaveFlakes = false;
+                          steps = dir:
+                            stepsIf ("${names.matrix.os} == '${os.ubuntu-22}'")
+                              (
+                                let writeDocsName = "Write docs"; in
+                                [
+                                  {
+                                    name = writeDocsName;
+                                    run = run.nixScript {
+                                      inherit dir;
+                                      name = packages1.writeDocs.pname;
+                                    };
+                                  }
+                                  {
+                                    name = "Commit & Push changes";
+                                    run = run.nix {
+                                      doCommit = true;
+                                      commitArgs.messages = [ (steps.format { }).name (steps.updateLocks { }).name writeDocsName ];
+                                    };
+                                  }
+                                ]
+                              )
+                            ++ [
+                              {
+                                name = "Build lima";
+                                run = run.nix_ { doInstall = true; scripts = [ "" "sdist" ]; };
+                              }
+                            ];
+                        };
+                      }
+                    );
+                };
+              in
+              packages1 // packages2;
+
+            devShells.default = mkShell {
+              packages = tools;
+              bash.extra = "export LC_ALL=C.UTF-8";
+              commands =
+                mkCommands "tools" tools
+                ++ mkRunCommands "ide" { "codium ." = packages.codium; inherit (packages) writeSettings; }
+                ++ [
+                  {
+                    name = "test";
+                    category = "test";
+                    help = "Build via `cabal`";
+                    command = "cabal v1-test";
+                  }
+                ];
             };
           in
           {
-            inherit packages;
-            params = { inherit override ghcVersion lima; };
+            inherit packages devShells;
+
           })
-        // {
-          inherit (inputs) formatter;
-        };
+        // { inherit (inputs) formatter; };
     in
     outputs;
 
